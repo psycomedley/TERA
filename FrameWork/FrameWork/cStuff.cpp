b8aa1768ef9c40b9ba3213800e7623429bc8bbdb
songhojung <shj6078@naver.com>
2016-12-19 11:49:03 +0900

    공호정 : 오브젝트툴매니저 수정

--- FrameWork/FrameWork/cStuff.cpp
+++ FrameWork/FrameWork/cStuff.cpp
@@ -3,11 +3,11 @@
 
 
 cStuff::cStuff(char* szFolder, char* szFilename)
-	:m_vPosition(0,0,0)
-	, m_vScale(0, 0, 0)
-	, m_fRotX(0.0f)
+	: m_fRotX(0.0f)
 	, m_fRotY(0.0f)
 	, m_fRotZ(0.0f)
+	, m_IsCullMode(false)
+	, m_SubSetNum(0)
 {
 	m_pMesh = new cStaticMesh(szFolder, szFilename);
 	D3DXMatrixIdentity(&m_matWorld);
@@ -37,11 +37,11 @@ void cStuff::Render()
 	g_pD3DDevice->SetRenderState(D3DRS_ALPHATESTENABLE, true);
 	g_pD3DDevice->SetRenderState(D3DRS_ALPHAREF, 0x00000088);
 	g_pD3DDevice->SetRenderState(D3DRS_ALPHAFUNC, D3DCMP_GREATER);
-
+	
 	D3DXMATRIXA16	pRot;
 	D3DXMatrixIdentity(&pRot);
-	D3DXMatrixTranslation(&m_matT, 0.0f, 0.0f, 10.0f);
-	D3DXMatrixScaling(&m_matS, 0.05f, 0.05f, 0.05f);
+	D3DXMatrixTranslation(&m_matT, m_vPosition.x, m_vPosition.y, m_vPosition.z);
+	D3DXMatrixScaling(&m_matS, m_vScale.x, m_vScale.y, m_vScale.z);
 	D3DXMatrixRotationX(&m_matRX, m_fRotX);
 	D3DXMatrixRotationY(&m_matRY, m_fRotY);
 	D3DXMatrixRotationZ(&m_matRZ, m_fRotZ);
@@ -50,8 +50,8 @@ void cStuff::Render()
 
 
 	g_pD3DDevice->SetTransform(D3DTS_WORLD, &m_matWorld);
-	((cStaticMesh*)m_pMesh)->SetIsCullMode(true);
-	((cStaticMesh*)m_pMesh)->SetDwSubSetNum(0);
+	((cStaticMesh*)m_pMesh)->SetIsCullMode(m_IsCullMode);
+	((cStaticMesh*)m_pMesh)->SetDwSubSetNum(m_SubSetNum);
 	cStaticObj::Render();
 	g_pD3DDevice->SetRenderState(D3DRS_LIGHTING, true);
 	g_pD3DDevice->SetRenderState(D3DRS_ALPHATESTENABLE, false);
