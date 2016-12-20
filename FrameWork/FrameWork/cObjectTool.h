d1b12e4463a555f5d0814f535c83f97bb13e5166
songhojung <shj6078@naver.com>
2016-12-20 11:47:43 +0900

    송호정: 오브젝트툴 (오브젝트 크기,회전,위치 변경기능추가)

--- FrameWork/FrameWork/cObjectTool.h
+++ FrameWork/FrameWork/cObjectTool.h
@@ -2,17 +2,22 @@
 
 #include "cStaticObj.h"
 
+
 class cObjectTool :public cStaticObj
 {
 private:
-	cStaticObj* m_BodyStuff;
-
+	cStaticObj*		m_BodyStuff;
+	int				m_RotDirection;
+	float			m_fRx, m_fRy, m_fRz;
+	D3DXVECTOR3		m_vScaling;
 public:
 	cObjectTool();
 	~cObjectTool();
 	
 	void Setup();
+	void ResetVariable();
 	void Update() override;
 	void ChangeBodyStuff();
+	void ChangeScaleAndAngle();
 };
 
