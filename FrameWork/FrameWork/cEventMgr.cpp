cb3b7ab550819a239fbb949b1ff22815997e7b82
PsycoMedley <briansara90@gmail.com>
2016-12-23 01:34:15 +0900

    홍기선 : eventmgr 추가

---
+++ FrameWork/FrameWork/cEventMgr.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "cEventMgr.h"
+
+
+cEventMgr::cEventMgr()
+{
+}
+
+
+cEventMgr::~cEventMgr()
+{
+}
---
+++ FrameWork/FrameWork/cEventMgr.h
@@ -0,0 +1,8 @@
+#pragma once
+class cEventMgr
+{
+public:
+	cEventMgr();
+	~cEventMgr();
+};
+
