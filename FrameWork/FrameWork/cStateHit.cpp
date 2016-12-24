9a2dfcb95f791a913d257030d569d0ff448385a8
PsycoMedley <briansara90@gmail.com>
2016-12-24 12:15:34 +0900

    홍기선 : hit state 추가

---
+++ FrameWork/FrameWork/cStateHit.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "cStateHit.h"
+
+
+cStateHit::cStateHit()
+{
+}
+
+
+cStateHit::~cStateHit()
+{
+}
---
+++ FrameWork/FrameWork/cStateHit.h
@@ -0,0 +1,8 @@
+#pragma once
+class cStateHit
+{
+public:
+	cStateHit();
+	~cStateHit();
+};
+
