305759cf1cf696ac5772b985efd64a6b3edcca51
PsycoMedley <briansara90@gmail.com>
2016-12-26 18:38:07 +0900

    홍기선 : 버그수정

---
+++ FrameWork/FrameWork/cEvent2.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "cEvent2.h"
+
+
+cEvent2::cEvent2()
+{
+}
+
+
+cEvent2::~cEvent2()
+{
+}
---
+++ FrameWork/FrameWork/cEvent2.h
@@ -0,0 +1,8 @@
+#pragma once
+class cEvent2
+{
+public:
+	cEvent2();
+	~cEvent2();
+};
+
