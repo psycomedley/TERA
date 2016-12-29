7caa624e9e62b66a8a763a64c7c958b164f9198d
songhojung <shj6078@naver.com>
2016-12-29 16:21:56 +0900

    송호정 :푸쉬

--- FrameWork/FrameWork/cMainGame.cpp
+++ FrameWork/FrameWork/cMainGame.cpp
@@ -111,8 +111,8 @@ HRESULT cMainGame::Setup()
 	D3DXMatrixRotationY(&matR, D3DX_PI / 2);
 	pPlayer->SetRevision(matR);
 	pPlayer->SetRevisionAngle(D3DX_PI / 2);
-	//pPlayer->SetPosition(D3DXVECTOR3(-140, 0, 80));
-	pPlayer->SetPosition(D3DXVECTOR3(140, 0, 80));
+	pPlayer->SetPosition(D3DXVECTOR3(-140, 0, 80));
+	//pPlayer->SetPosition(D3DXVECTOR3(140, 0, 80));
 	GETSINGLE(cObjMgr)->SetPlayer(pPlayer);
 
 	cDynamicObj* pBoss = new cOrca("Monster", "Orca.X");
